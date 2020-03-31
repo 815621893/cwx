32957bf640e35a0f678265d2e373682e4f35daeb
815621893 <62938926+815621893@users.noreply.github.com>
2020-03-31 22:59:13 +0800

    上传文件
    上传一个类

---
+++ A.java
@@ -0,0 +1,25 @@
+  public class A
+{
+	    public static void b(int o){
+		 System.out.println(o);
+	    }
+		 public static void b(byte o){
+		 System.out.println(o);
+	    }
+		 public static void b(float o){
+		 System.out.println(o);
+	    }
+		 public static void b(boolean o){
+		 System.out.println(o);
+	    }
+		 public static void b(short o){
+		 System.out.println(o);
+	    }
+		 public static void b(long o){
+		 System.out.println(o);
+	    }
+		public static void b(double o){
+		 System.out.println(o);
+	    }
+		
+}
